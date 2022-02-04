16ae888e2697e5eabbe37857cf4fe4ee1bd9a625
Joachim Køcher Kelsen <joachim@kelsen.nu>
2022-02-04 09:56:25 +0100

    add: gateway to prometheus

--- GatewayService/Program.cs
+++ GatewayService/Program.cs
@@ -1,8 +1,32 @@
+using Serilog;
+using Serilog.Events;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add serilog
+builder.Host.UseSerilog((context, serviceProvider, config) =>
+{
+    var seqUri = context.Configuration["Logging:SeqUri"];
+    config.WriteTo.Seq(seqUri)
+        .Enrich.FromLogContext()
+        .MinimumLevel.Override("GatewayService", LogEventLevel.Information)
+        .MinimumLevel.Warning();
+});
 
 //ADD SERVICES
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Strict", policyBuilder =>
+    {
+        policyBuilder.AllowCredentials();
+        policyBuilder.WithOrigins();
+        policyBuilder.AllowAnyMethod();
+        policyBuilder.AllowAnyHeader();
+    });
+});
 
 //PIPELINE SETUP
 var app = builder.Build();
