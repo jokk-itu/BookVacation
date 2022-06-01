f453343438caad0407db0cd4757b3eaf253def5e
Joachim Køcher Kelsen <joachim@kelsen.nu>
2022-06-01 21:12:51 +0200

    add: ticketservice to terraform

---
+++ Terraform/modules/ticketservice/variables.tf
@@ -0,0 +1,43 @@
+variable "cluster_certificate" {
+  type = string
+}
+
+variable "cluster_token" {
+  type = string
+}
+
+variable "cluster_host" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "tls-secretname" {
+  type = string
+}
+
+variable "logger-configname" {
+  type = string
+}
+
+variable "eventbus-secretname" {
+  type = string
+}
+
+variable "minio-secretname" {
+  type = string
+}
+
+variable "minio-configname" {
+  type = string
+}
+
+variable "cert-issuername" {
+  type = string
+}
