3aab0faf40b7610410a27320e50628e3fb724c8b
Joachim Køcher Kelsen <joachim@kelsen.nu>
2022-06-04 09:11:20 +0200

    add: requesthandler and controller tests

---
+++ VacationService/VacationService.Tests/CreateVacationRequestHandlerTest.cs
@@ -0,0 +1,42 @@
+using MassTransit.Testing;
+using VacationService.Infrastructure.Requests.CreateVacation;
+using Xunit;
+
+namespace VacationService.Test;
+
+public class CreateVacationRequestHandlerTest
+{
+    [Trait("Category", "Unit")]
+    [Fact]
+    public async Task Handle_ExpectSentRoutingSlip()
+    {
+        //Arrange
+        var harness = new InMemoryTestHarness();
+        var request = new CreateVacationRequest(
+            Guid.NewGuid(),
+            Guid.NewGuid(), 
+            Guid.NewGuid(),
+            DateTimeOffset.Now,
+            DateTimeOffset.Now, 
+            Guid.NewGuid(),
+            Guid.NewGuid(),
+            string.Empty,
+            DateTimeOffset.Now,
+            DateTimeOffset.Now);
+
+        //Act
+        await harness.Start();
+        try
+        {
+            var requestHandler = new CreateVacationRequestHandler(harness.BusControl);
+            await requestHandler.Handle(request, CancellationToken.None);
+            
+            //Assert
+            Assert.True(await harness.Sent.Any());
+        }
+        finally
+        {
+            await harness.Stop();
+        }
+    }
+}
